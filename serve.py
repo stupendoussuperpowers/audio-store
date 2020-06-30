e4898c7dc5cc184d02df47a62750fa6ea844e910
sanchit sahay <sanchitsahay.tech@gmail.com>
2020-06-30 21:13:44 +0530

    does the thing, I guess

--- serve.py
+++ serve.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+import os
 
 # creates a Flask application, named app
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "/tmp/"
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/")
 def hello():
     message = "Hello, World"
-    return render_template('index.html', message=message)
+    return render_template('audio.html')
+
+@app.route("/upload", methods= ['POST'])
+def upload():
+	print(request)
+	print(request.files["audio"])
+	file = request.files["audio"]
+	file.save(os.path.join(UPLOAD_FOLDER, file.filename + ".wav"))
+	return "<h1> okay </h1>"
 
 # run the application
 if __name__ == "__main__":
